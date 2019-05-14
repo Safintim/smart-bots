261268562108f11c1500ce2bbfb0120ad1b1037e
Safintim <Sarami96>
2019-05-14 11:36:41 +0300

    Created tools for dialogflow

---
+++ dialoflow_tools.py
@@ -0,0 +1,43 @@
+import dialogflow_v2 as dialogflow
+
+
+def get_response_from_dialogflow(project_id, session_id, text):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.types.TextInput(text=text, language_code='ru')
+    query_input = dialogflow.types.QueryInput(text=text_input)
+    response = session_client.detect_intent(session=session, query_input=query_input)
+
+    return response
+
+
+def list_intents(project_id):
+    intents_client = dialogflow.IntentsClient()
+    parent = intents_client.project_agent_path(project_id)
+    intents = intents_client.list_intents(parent)
+
+    return [intent.display_name for intent in intents]
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    intents_client = dialogflow.IntentsClient()
+
+    parent = intents_client.project_agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.types.Intent.TrainingPhrase.Part(
+            text=training_phrases_part)
+        training_phrase = dialogflow.types.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.types.Intent.Message.Text(text=[message_texts])
+    message = dialogflow.types.Intent.Message(text=text)
+
+    intent = dialogflow.types.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message])
+
+    response = intents_client.create_intent(parent, intent)
+    print('Intent created: {}'.format(response))
+
