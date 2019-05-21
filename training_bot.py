e1d5c192af59c40041ab77fca11631189b63e5f5
Safintim <Sarami96>
2019-05-21 13:54:53 +0300

    Refactoring - deleted prints in function

--- training_bot.py
+++ training_bot.py
@@ -4,11 +4,13 @@ from dialoflow_tools import list_intents, create_intent
 from dotenv import load_dotenv
 
 
+def is_intent(intent):
+    return not isinstance(intent, str)
+
+
 def read_training_file(training_file):
     with open(training_file) as json_file:
-        training_phrases_parts = json.load(json_file)
-
-    return training_phrases_parts
+        return json.load(json_file)
 
 
 def train_bot(training_phrases_parts, project_id):
@@ -23,13 +25,31 @@ def train_bot(training_phrases_parts, project_id):
 
 def main():
     load_dotenv()
+
     training_phrases_parts = read_training_file('questions.json')
     for intent in train_bot(training_phrases_parts, os.getenv('PROJECT_ID')):
-        if intent:
-            print('Intent created: {}'.format(intent))
+        if is_intent(intent):
+            print('Intent created: {}'.format(intent.display_name))
         else:
-            print(intent, 'is exist')
+            print('{} is exist'.format(intent))
 
 
 if __name__ == '__main__':
     main()
+
+
+    # def form_intent(training_phrases_parts):
+    #     return ((display_name, questions_and_answers['questions'], questions_and_answers['answer'])
+    #             for display_name, questions_and_answers in training_phrases_parts.items())
+
+
+
+    # project_id = os.getenv("PROJECT_ID")
+    # training_phrases_parts = read_training_file('questions.json')
+    # existing_intents = list_intents(project_id)
+    # for display_name, questions, answer in form_intent(training_phrases_parts):
+    #     if display_name in existing_intents:
+    #         print(f'{display_name} is exist')
+    #     else:
+    #         response = create_intent(project_id, display_name, questions, answer)
+    #         print(f'{"-" * 3} Intent created: {response.display_name}')
